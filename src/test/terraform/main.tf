bce0118a12c830be1c5388e5e33264b7ff81afb2
alandevoteam <130046939+alandevoteam@users.noreply.github.com>
2023-06-06 15:40:57 +0200

    Update main.tf

--- src/test/terraform/main.tf
+++ src/test/terraform/main.tf
@@ -322,142 +322,142 @@ resource "azurerm_dev_test_global_vm_shutdown_schedule" "accept" {
 
 
 
-# # 4 virtual network
-resource "azurerm_virtual_network" "production" {
-  name                = "production-vnet"
-  location            = azurerm_resource_group.test.location
-  resource_group_name = azurerm_resource_group.test.name
-  address_space       = ["10.2.0.0/16"]
-  tags = {
-    Pillar = "M Cloud"
-    Role   = "Futures"
-    Usage  = "Training / Certification related activities"
-  }
-}
+# # # 4 virtual network
+# resource "azurerm_virtual_network" "production" {
+#   name                = "production-vnet"
+#   location            = azurerm_resource_group.test.location
+#   resource_group_name = azurerm_resource_group.test.name
+#   address_space       = ["10.2.0.0/16"]
+#   tags = {
+#     Pillar = "M Cloud"
+#     Role   = "Futures"
+#     Usage  = "Training / Certification related activities"
+#   }
+# }
 
-# 5 subnet
-resource "azurerm_subnet" "production" {
-  name                 = "production-subnet"
-  resource_group_name  = azurerm_resource_group.test.name
-  virtual_network_name = azurerm_virtual_network.production.name
-  address_prefixes     = ["10.2.0.0/24"]
-}
+# # 5 subnet
+# resource "azurerm_subnet" "production" {
+#   name                 = "production-subnet"
+#   resource_group_name  = azurerm_resource_group.test.name
+#   virtual_network_name = azurerm_virtual_network.production.name
+#   address_prefixes     = ["10.2.0.0/24"]
+# }
 
-# 6 network security group
-resource "azurerm_network_security_group" "production" {
-  name                = "production-nsg"
-  location            = azurerm_resource_group.test.location
-  resource_group_name = azurerm_resource_group.test.name
-  tags = {
-    Pillar = "M Cloud"
-    Role   = "Futures"
-    Usage  = "Training / Certification related activities"
-  }
-}
+# # 6 network security group
+# resource "azurerm_network_security_group" "production" {
+#   name                = "production-nsg"
+#   location            = azurerm_resource_group.test.location
+#   resource_group_name = azurerm_resource_group.test.name
+#   tags = {
+#     Pillar = "M Cloud"
+#     Role   = "Futures"
+#     Usage  = "Training / Certification related activities"
+#   }
+# }
 
-# 7 network security rule
-resource "azurerm_network_security_rule" "production" {
-  name                        = "production-rule"
-  priority                    = 100
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "*"
-  source_port_range           = "*"
-  destination_port_range      = "*"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-  resource_group_name         = azurerm_resource_group.test.name
-  network_security_group_name = azurerm_network_security_group.production.name
-}
+# # 7 network security rule
+# resource "azurerm_network_security_rule" "production" {
+#   name                        = "production-rule"
+#   priority                    = 100
+#   direction                   = "Inbound"
+#   access                      = "Allow"
+#   protocol                    = "*"
+#   source_port_range           = "*"
+#   destination_port_range      = "*"
+#   source_address_prefix       = "*"
+#   destination_address_prefix  = "*"
+#   resource_group_name         = azurerm_resource_group.test.name
+#   network_security_group_name = azurerm_network_security_group.production.name
+# }
 
-# 8 network subnet nsg asso
-resource "azurerm_subnet_network_security_group_association" "production" {
-  subnet_id                 = azurerm_subnet.production.id
-  network_security_group_id = azurerm_network_security_group.production.id
-}
+# # 8 network subnet nsg asso
+# resource "azurerm_subnet_network_security_group_association" "production" {
+#   subnet_id                 = azurerm_subnet.production.id
+#   network_security_group_id = azurerm_network_security_group.production.id
+# }
 
-# 9 public ip
-resource "azurerm_public_ip" "production" {
-  name                = "production-pip"
-  resource_group_name = azurerm_resource_group.test.name
-  location            = azurerm_resource_group.test.location
-  allocation_method   = "Static"
-  domain_name_label   = "prodpetclinic"
-  tags = {
-    Pillar = "M Cloud"
-    Role   = "Futures"
-    Usage  = "Training / Certification related activities"
-  }
-}
+# # 9 public ip
+# resource "azurerm_public_ip" "production" {
+#   name                = "production-pip"
+#   resource_group_name = azurerm_resource_group.test.name
+#   location            = azurerm_resource_group.test.location
+#   allocation_method   = "Static"
+#   domain_name_label   = "prodpetclinic"
+#   tags = {
+#     Pillar = "M Cloud"
+#     Role   = "Futures"
+#     Usage  = "Training / Certification related activities"
+#   }
+# }
 
-# 10 network interface
-resource "azurerm_network_interface" "production" {
-  name                = "production-nic"
-  location            = azurerm_resource_group.test.location
-  resource_group_name = azurerm_resource_group.test.name
-  tags = {
-    Pillar = "M Cloud"
-    Role   = "Futures"
-    Usage  = "Training / Certification related activities"
-  }
+# # 10 network interface
+# resource "azurerm_network_interface" "production" {
+#   name                = "production-nic"
+#   location            = azurerm_resource_group.test.location
+#   resource_group_name = azurerm_resource_group.test.name
+#   tags = {
+#     Pillar = "M Cloud"
+#     Role   = "Futures"
+#     Usage  = "Training / Certification related activities"
+#   }
 
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.production.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.production.id
-  }
-}
+#   ip_configuration {
+#     name                          = "internal"
+#     subnet_id                     = azurerm_subnet.production.id
+#     private_ip_address_allocation = "Dynamic"
+#     public_ip_address_id          = azurerm_public_ip.production.id
+#   }
+# }
 
-# 11 vm
-resource "azurerm_linux_virtual_machine" "production" {
-  name                  = "production-vm"
-  resource_group_name   = azurerm_resource_group.test.name
-  location              = azurerm_resource_group.test.location
-  size                  = "Standard_D2s_v3"
-  admin_username        = "adminuser"
-  network_interface_ids = [azurerm_network_interface.production.id]
-  tags = {
-    Pillar = "M Cloud"
-    Role   = "Futures"
-    Usage  = "Training / Certification related activities"
-  }
+# # 11 vm
+# resource "azurerm_linux_virtual_machine" "production" {
+#   name                  = "production-vm"
+#   resource_group_name   = azurerm_resource_group.test.name
+#   location              = azurerm_resource_group.test.location
+#   size                  = "Standard_D2s_v3"
+#   admin_username        = "adminuser"
+#   network_interface_ids = [azurerm_network_interface.production.id]
+#   tags = {
+#     Pillar = "M Cloud"
+#     Role   = "Futures"
+#     Usage  = "Training / Certification related activities"
+#   }
 
-  admin_ssh_key {
-    username   = "adminuser"
-    public_key = file("/home/alan/.ssh/productionvm.pub")
-  }
+#   admin_ssh_key {
+#     username   = "adminuser"
+#     public_key = file("/home/alan/.ssh/productionvm.pub")
+#   }
 
-  os_disk {
-    name                 = "productionOsDisk"
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
+#   os_disk {
+#     name                 = "productionOsDisk"
+#     caching              = "ReadWrite"
+#     storage_account_type = "Standard_LRS"
+#   }
 
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-focal"
-    sku       = "20_04-lts-gen2"
-    version   = "20.04.202209200"
-  }
-}
+#   source_image_reference {
+#     publisher = "Canonical"
+#     offer     = "0001-com-ubuntu-server-focal"
+#     sku       = "20_04-lts-gen2"
+#     version   = "20.04.202209200"
+#   }
+# }
 
-resource "azurerm_dev_test_global_vm_shutdown_schedule" "production" {
-  virtual_machine_id = azurerm_linux_virtual_machine.production.id
-  location           = azurerm_resource_group.test.location
-  enabled            = true
-  tags = {
-    Pillar = "M Cloud"
-    Role   = "Futures"
-    Usage  = "Training / Certification related activities"
-  }
+# resource "azurerm_dev_test_global_vm_shutdown_schedule" "production" {
+#   virtual_machine_id = azurerm_linux_virtual_machine.production.id
+#   location           = azurerm_resource_group.test.location
+#   enabled            = true
+#   tags = {
+#     Pillar = "M Cloud"
+#     Role   = "Futures"
+#     Usage  = "Training / Certification related activities"
+#   }
 
 
-  daily_recurrence_time = "1900"
-  timezone              = "W. Europe Standard Time"
+#   daily_recurrence_time = "1900"
+#   timezone              = "W. Europe Standard Time"
 
-  notification_settings {
-    enabled = false
+#   notification_settings {
+#     enabled = false
 
-  }
-}
+#   }
+# }
