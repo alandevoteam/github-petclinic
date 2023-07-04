409f08b94cc197b3e79e093cdc04ae0f6b67577a
Alan Ahmad <130046939+alandevoteam@users.noreply.github.com>
2023-07-04 21:33:37 +0200

    Update main.tf

--- src/test/terraform/main.tf
+++ src/test/terraform/main.tf
@@ -381,9 +381,9 @@ resource "azurerm_network_security_rule" "production" {
   priority                    = 100
   direction                   = "Inbound"
   access                      = "Allow"
-  protocol                    = "*"
+  protocol                    = "TCP"
   source_port_range           = "*"
-  destination_port_range      = "*"
+  destination_port_range      = "80"
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
   resource_group_name         = azurerm_resource_group.test.name
